188e133be5399dbb4be4d4a7077fb3399be51c36
Pedro Jesus <pedrojesus.cefet@gmail.com>
2018-01-27 15:03:04 -0200

    Adicionar arquivos de projeto.

---
+++ SplashScreen/SplashScreen/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using SplashScreen.ViewModel;
+using Xamarin.Forms;
+
+namespace SplashScreen
+{
+    public partial class MainPage : ContentPage
+	{
+        MainViewModel Vm = new MainViewModel();
+		public MainPage()
+		{
+			InitializeComponent();
+            BindingContext = Vm;
+            
+		}
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await Vm.Carregar();
+        }
+    }
+}
---
+++ SplashScreen/SplashScreen/ViewModel/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplashScreen.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private bool _isBusy;
+
+        public bool IsBusy
+        {
+            get { return _isBusy; }
+            set {SetProperty(ref _isBusy , value); }
+        }
+        private string _msg;
+
+        public string Msg
+        {
+            get { return _msg; }
+            set {SetProperty(ref _msg , value); }
+        }
+
+
+        public async Task Carregar()
+        {
+            Msg = "Carregando";
+            IsBusy = true;
+            await Task.Delay(9000);
+            IsBusy = false;
+            Msg = "Finalizado Carregamento";
+        }
+    }
+}
